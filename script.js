91a3c29b326afcd6d2c790bda931d7e87a075ba5
WinterBlues1220 <131233362+WinterBlues1220@users.noreply.github.com>
2026-06-15 11:03:32 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 const toggleButton = document.getElementById('theme-toggle');
+const themeIcon = document.getElementById('theme-icon');
+
 toggleButton.addEventListener('click', () => {
     document.body.classList.toggle('dark');
+    
+    if (document.body.classList.contains('dark')) {
+        themeIcon.setAttribute('name', 'sunny-outline');
+    } else {
+        themeIcon.setAttribute('name', 'moon-outline');
+    }
 });
